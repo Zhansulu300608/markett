af7d29bb1eaafd13c49bb77f066db933d134f20f
sabinurr <zhansulu.mukhamedsapa@jihc.edu.kz>
2025-12-13 16:10:41 +0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <header class="w-full bg-[url('/images/newYear.3940986.png')] bg-contain bg-no-repeat
+  bg-center h-40 text-white shadow-md relative overflow-hidden">
+    <div class="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
+      <!-- Left Section -->
+      <div class="flex items-center space-x-6">
+        <!-- Burger Menu -->
+        <!-- <button class="text-2xl">
+          <span class="material-icons">menu</span>
+        </button> -->
+
+        <!-- Logo -->
+        <div class="flex items-center space-x-2 text-[#003049]">
+          <img src="../public/images/logo.png" alt="Logo" class="h-10" />
+         
+        </div>
+      </div>
+
+      <!-- Search Bar -->
+      <div class="flex-1 mx-10 mt-5">
+        <div class="flex rounded-full overflow-hidden shadow-sm">
+          <input
+            type="text"
+            placeholder="Поиск"
+            class="flex-1 px-4 py-2 text-gray-700 focus:outline-none"
+          />
+          <button class="px-4 text-gray-600 border-l">по скидкам</button>
+        </div>
+      </div>
+
+      <!-- Location / Lang -->
+      <div class="flex items-center space-x-4">
+        <button class="bg-white text-gray-800 px-4 py-2 rounded-full flex items-center space-x-2 shadow">
+        <NuxtLink to="/login" class="login-btn">Login</NuxtLink>
+        </button>
+        <button class="bg-white text-gray-800 px-4 py-2 rounded-full flex items-center space-x-2 shadow">
+          <span>RU</span>
+        </button>
+      </div>
+    </div>
+
+    <!-- Bottom Nav -->
+    <nav class=" text-gray-800 py-4 mt-5 shadow-md">
+      <div class="max-w-7xl mx-auto px-4 flex space-x-8 text-sm font-medium">
+         <a href="#" class="hover:text-[#C1121F]">Главная</a>
+        <a href="#" class="hover:text-[#C1121F]">Каталог</a>
+        <a href="#" class="hover:text-[#C1121F]">Доставка</a>
+        <a href="#" class="hover:text-[#C1121F]">Профиль</a>
+         <a href="#" class="hover:text-[#C1121F]">О компании</a>
+        <a href="#" class="hover:text-[#C1121F]">Контакты</a>
+      </div>
+    </nav>
+  </header>
+
+  <div data-v-9aedfe68="" class="home-banner__hashtags"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/milka" target="_blank" class="home-banner__hashtags-btn">
+        #Выиграй автомобиль с Milka
+      </a><a data-v-9aedfe68="" href="https://magnum.kz/stocks/podarok-barni" target="_blank" class="home-banner__hashtags-btn">
+        #Подарок от Barni
+      </a><a data-v-9aedfe68="" href="https://partyclub.kz/ru/stocks/105" target="_blank" class="home-banner__hashtags-btn">
+        #+21 Бонусы от Reyka
+      </a></div>
+      <div data-v-9aedfe68="" class="home-banner__slider"><div data-v-9aedfe68="" class="swiper-container swiper-container-initialized swiper-container-horizontal swiper-container-ios"><div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(-4180px, 0px, 0px);"><div data-v-9aedfe68="" class="swiper-slide swiper-slide-duplicate" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="17"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/vy-pomogaete-vdohnovlyat-detej-na-zdorovoe-pitanie-pokupaya-bonduelle-v-noyabre"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/576x540_5_408e2eec45.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="0"><a data-v-9aedfe68="" href="https://magnum.kz/kolesopodarkov"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/RU_576x540_25ffc8fc32.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="1"><a data-v-9aedfe68="" href="https://magnum.kz/zhanazhylmagnum"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/Banner_3_7fe1cd936b.png" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="2"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/magnum-paida"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/576x540_RU_9ad88aa76e.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="3"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/milka"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/576h540_96d1903e2d.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="4"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/ty-ne-ty-kogda-goloden"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/SNK_web_576x540_rus_3f115609a9.png" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="5"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/nivea-and-bosch"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/KV_576_H540_kopiya_608655c02c.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="6"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/mars_promo"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/SNK_Kairat_Magnum_Web_mobile_akcii_576x540_rus_7890c7d8ac.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="7"><a data-v-9aedfe68="" href="https://freedombank.onelink.me/WNLd/ic2fbzpo"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/Mobile_576x540_855e2a128a.png" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="8"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/visa-olimpijskie-igry"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/RU_576x540_4051db511f.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide swiper-slide-prev" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="9"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/kunde-december2025"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/576x540_RU_f7d0c49a93.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide swiper-slide-active" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="10"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/promocode-bonduelle"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/576x540_5_408e2eec45.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide swiper-slide-next" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="11"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/vkusnoe-kombo2"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/576h540_kopiya_9e8ff069ae.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="12"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/novii-god2025"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/NF_NY_banners576x540_RU_9ae33b2b52.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="13"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/lovite-volshebstvo"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/Juzzon_AVA_576h540_rus_7b2e89c89b.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="14"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/podarok-barni"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/mob_akciyi_ru_576x540_9528489168.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="15"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/orbit-ponedelnik-den-ch-illovyj?"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/576h540_ru_1_d440c5374e.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="16"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/skidkimagnumchef"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/2_Kulinariya_25_576x540_288x270_rus_9ae26d0b77.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="17"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/vy-pomogaete-vdohnovlyat-detej-na-zdorovoe-pitanie-pokupaya-bonduelle-v-noyabre"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/576x540_5_408e2eec45.jpg" alt="" class="home-banner__slider-images"></a></div><div data-v-9aedfe68="" class="swiper-slide swiper-slide-duplicate" style="width: 360px; margin-right: 20px;" data-swiper-slide-index="0"><a data-v-9aedfe68="" href="https://magnum.kz/kolesopodarkov"><img data-v-9aedfe68="" src="https://magnum.kz:1337/uploads/RU_576x540_25ffc8fc32.jpg" alt="" class="home-banner__slider-images"></a></div></div></div> <div data-v-9aedfe68="" class="home-banner__slider-bottom"><div data-v-9aedfe68="" class="home-banner__slider-bottom--box"><a data-v-9aedfe68="" href="https://magnum.kz/stocks/promocode-bonduelle" class="home-banner__btn home-banner__slider-btn">Подробнее</a> <p data-v-9aedfe68="" class="home-banner__slider-pages">
+              11/18
+            </p></div> <div data-v-9aedfe68="" class="home-banner__slider-arrows"><button data-v-9aedfe68="" class="home-banner__slider-arr prev"></button> <button data-v-9aedfe68="" class="home-banner__slider-arr next"></button></div> <!----> <!----></div></div>
+            
+</template>
+
+<script setup>
+// No logic needed for static header
+</script>
+
+<style scoped>
+/* You may replace the material icons with your own icon library */
+/* @import url('https://fonts.googleapis.com/icon?family=Material+Icons'); */
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="bg-gray-100 text-gray-900 min-h-screen flex items-center justify-center p-6">
+
+    <div class="w-full max-w-md p-10 rounded-xl shadow-lg relative bg-white border border-purple-200">
+
+      <!-- Logo -->
+      <div class="flex items-center justify-center gap-3 mb-6">
+      <img src="../public/images/logoo.png" alt="Logo" class="h-10" />
+      </div>
+
+      <!-- Title -->
+      <h1 class="text-3xl font-bold leading-tight mb-2 text-center">
+        Добро пожаловать<br />обратно!
+      </h1>
+
+      <p class="text-gray-500 text-center mb-6">
+        Войдите в свой аккаунт, чтобы продолжить.
+      </p>
+
+      <!-- Errors -->
+      <p
+        v-if="error"
+        class="text-red-500 text-center mb-2 font-semibold"
+      >{{ error }}</p>
+
+      <p
+        v-if="success"
+        class="text-green-500 text-center mb-2 font-semibold"
+      >{{ success }}</p>
+
+      <!-- Form -->
+      <form @submit.prevent="handleSubmit" class="flex flex-col gap-3">
+        <label class="font-semibold">Электронная почта</label>
+        <input
+          type="email"
+          v-model="form.email"
+          placeholder="ваша@почта.рф"
+          class="p-3 rounded-lg border border-gray-300 bg-transparent focus:border-[#C1121F] outline-none"
+        />
+
+        <label class="font-semibold">Пароль</label>
+        <input
+          type="password"
+          v-model="form.password"
+          placeholder="********"
+          class="p-3 rounded-lg border border-gray-300 bg-transparent focus:border-[#C1121F] outline-none"
+        />
+
+        <button
+          type="submit"
+          class="mt-3 bg-[#C1121F] hover:bg-[#780000] text-white py-3 rounded-lg font-semibold transition"
+        >
+          Войти
+        </button>
+      </form>
+
+      <p class="mt-5 text-center text-sm">
+        Еще нет аккаунта?
+        <NuxtLink to="/register" class="text-[#C1121F] hover:underline font-semibold">
+          Зарегистрироваться
+        </NuxtLink>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const form = reactive({
+  email: "",
+  password: "",
+});
+
+const error = ref("");
+const success = ref("");
+
+// LOGIN
+const handleSubmit = async () => {
+  error.value = "";
+  success.value = "";
+
+  try {
+    const res = await axios.post(
+      "https://medical-backend-54hp.onrender.com/api/auth/login",
+      form
+    );
+
+    success.value = "Got it! You are logged in.";
+
+    if (res.data.token) {
+      localStorage.setItem("token", res.data.token);
+    }
+
+    form.email = "";
+    form.password = "";
+
+    setTimeout(() => {
+      router.push("/home");
+    }, 1500);
+
+  } catch (e) {
+    error.value = e?.response?.data?.message || "Login failed";
+  }
+};
+</script>
