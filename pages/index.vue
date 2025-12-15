e31d372637b3e01afbe0930faaf832fb3d2f9f87
sabinurr <zhansulu.mukhamedsapa@jihc.edu.kz>
2025-12-16 04:57:23 +0500

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,46 @@
 <template>
- <header
-    class="w-full bg-[url('/images/newYear.3940986.png')] bg-contain bg-no-repeat bg-center h-40 shadow-md relative"
-  >
+  <header class="w-full bg-[url('/images/newYear.3940986.png')] bg-contain bg-no-repeat
+  bg-center h-40 text-white shadow-md relative overflow-hidden">
     <div class="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
-      
-      <!-- Logo -->
-      <NuxtLink to="/" class="flex items-center space-x-2">
-        <img src="/images/logo.png" alt="Logo" class="h-10" />
-      </NuxtLink>
+      <!-- Left Section -->
+      <div class="flex items-center space-x-6">
+        <!-- Burger Menu -->
+        <!-- <button class="text-2xl">
+          <span class="material-icons">menu</span>
+        </button> -->
 
-      <!-- Search -->
+        <!-- Logo -->
+        <div class="flex items-center space-x-2 text-[#003049]">
+          <img src="../public/images/logo.png" alt="Logo" class="h-10" />
+         
+        </div>
+      </div>
+
+      <!-- Search Bar -->
       <div class="flex-1 mx-10 mt-5">
-        <div class="flex rounded-full overflow-hidden shadow-sm bg-white">
+        <div class="flex rounded-full overflow-hidden shadow-sm">
           <input
             type="text"
             placeholder="Поиск"
             class="flex-1 px-4 py-2 text-gray-700 focus:outline-none"
           />
-          <button class="px-4 text-white bg-[#003049]">
-            по скидкам
-          </button>
+           <button class="px-4 text-white bg-[#003049] border-l">по скидкам</button>
         </div>
       </div>
 
-      <!-- Auth / Lang -->
-      <div class="flex items-center gap-4">
-        
-        <!-- ЕСЛИ ЛОГИН БОЛСА -->
-        <NuxtLink
-          v-if="isAuth"
-          to="/profile"
-          class="flex items-center gap-2 bg-white px-4 py-2 rounded-full shadow hover:bg-gray-100 transition"
-        >
-          <!-- User Icon -->
-          <div
-            class="w-8 h-8 rounded-full bg-orange-500 text-white flex items-center justify-center font-bold"
-          >
-            {{ user.name?.charAt(0).toUpperCase() }}
-          </div>
-
-          <span class="hidden sm:block text-gray-800 font-medium">
-            {{ user.name }}
-          </span>
-        </NuxtLink>
-
-        <!-- ЕСЛИ ЛОГИН ЖОҚ -->
-        <NuxtLink
-          v-else
-          to="/login"
-          class="bg-white text-gray-800 px-4 py-2 rounded-full shadow hover:bg-gray-100"
-        >
-          Login
-        </NuxtLink>
-
-        <!-- Language -->
-       
+      <!-- Location / Lang -->
+      <div class="flex items-center space-x-4">
+        <button class="bg-white text-gray-800 px-4 py-2 rounded-full flex items-center space-x-2 shadow">
+        <NuxtLink to="/login" class="login-btn text-[#003049]">Login</NuxtLink>
+        </button>
+        <button class="bg-white text-gray-800 px-4 py-2 rounded-full flex items-center space-x-2 shadow">
+          <span>RU</span>
+        </button>
       </div>
     </div>
 
-    <!-- Navigation -->
-     <nav class=" text-gray-800 py-4 mt-5 shadow-md">
+    <!-- Bottom Nav -->
+    <nav class=" text-gray-800 py-4 mt-5 shadow-md">
       <div class="max-w-7xl mx-auto px-4 flex space-x-8 text-sm font-medium">
            <NuxtLink :to="{ name: 'glav' }" class="hover:text-[#C1121F] text-[#003049]">
          Главная
@@ -67,7 +48,7 @@
           <NuxtLink :to="{ name: 'catalog' }" class="hover:text-[#C1121F] text-[#003049]">
          Каталог
         </NuxtLink>
-      
+       
          <NuxtLink :to="{ name: 'profile' }" class="hover:text-[#C1121F] text-[#003049]">
          Профиль
         </NuxtLink>
@@ -356,19 +337,6 @@ onUnmounted(() => {
   clearInterval(interval);
 });
 
-
-const user = ref<any>({});
-const isAuth = ref(false);
-
-onMounted(() => {
-  const token = localStorage.getItem("token");
-  const savedUser = localStorage.getItem("user");
-
-  if (token && savedUser) {
-    user.value = JSON.parse(savedUser);
-    isAuth.value = true;
-  }
-});
 const items = ref([
   {
     label: "Кондитерская",
